8289a81f2a7285d2b815c405d22dd60bc912438d
weileng_Tv <weileng143@163.com>
2016-05-24 10:11:49 +0800

    Tv  飞框动画学习

---
+++ app/src/main/java/com/iptv/test/adapter/FragAdapter.java
@@ -0,0 +1,42 @@
+package com.iptv.test.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
+
+import com.iptv.test.fragment.BaseFragment;
+
+import java.util.List;
+
+public class FragAdapter extends FragmentPagerAdapter {
+
+    private List<BaseFragment> fragments;
+
+
+    public FragAdapter(FragmentManager fm, List<BaseFragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        return super.instantiateItem(container, position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        super.destroyItem(container, position, object);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+}  
